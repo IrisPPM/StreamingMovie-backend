7ae4a98212e923390f8266bdc5b06e3373861a00
Iris <irisppm@gmail.com>
2023-01-23 19:54:03 -0600

    create model user

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -9,10 +9,15 @@ const Query= {
     async getMoviesById(_,{_id}){
         const Movies = await Movie.findById(_id)
         return Movies
+    },
+
+    async login( _, { email, password } ) {
+        const verifyUser = await User.find( { email: email, password: password } );
+        console.log(verifyUser)
+        return verifyUser
     }
+    //Pruebas en playground con login en el minuto 48 de la clase del 7 de dic
 
-
-  
 
 }
 export default Query
